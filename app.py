4e8d33f94936afa03900cabf2b3c64c546f6524a
Mustehsan Nisar Rao <147530703+Mustehsan-Nisar-Rao@users.noreply.github.com>
2025-09-28 00:21:49 +0500

    Update app.py

--- app.py
+++ app.py
@@ -261,15 +261,8 @@ def translate_sentence(sentence, max_len=50):
                 input_tensor = torch.LongTensor([pred_token]).to(DEVICE)
         
         # Decode the tokens to text
-        # Safety: cut off anything after EOS
-                if sp.eos_id() in translated_tokens:
-                    eos_index = translated_tokens.index(sp.eos_id())
-                    translated_tokens = translated_tokens[:eos_index]
-                
-                # Decode the tokens to text
                 translated_text = sp.decode(translated_tokens) if translated_tokens else ""
-                return translated_text.strip()
-
+                return translated_text
         
     except Exception as e:
         return f"Translation error: {e}"
